5006f136ce6c286c3bbf7900969f5c0ac605a2cb
RafaelFusco <rafaelfusco2000@gmail>
2023-05-23 16:55:17 -0300

    coomit

--- src/components/MainPage/items.ts
+++ src/components/MainPage/items.ts
@@ -1,14 +1,13 @@
 
-
 export const itemsPizza = [
-    {id: 1, name: 'Calabresa', img:'../public/calabresa.png', price: 32.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id: 2, name: 'Margarita', img:'../public/margherita.png', price: 28.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id: 3, name: 'Pepperoni', img:'../public/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id: 4, name: 'Pepperoni', img:'../public/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id: 5, name: 'Pepperoni', img:'../public/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id: 6, name: 'Pepperoni', img:'../public/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id: 7, name: 'Pepperoni', img:'../public/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id: 8, name: 'Pepperoni', img:'../public/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id: 9, name: 'Pepperoni', img:'../public/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id: 10, name: 'Pepperoni', img:'../public/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id: 1, name: 'Calabresa', img:'../../assets/calabresa.png', price: 32.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id: 2, name: 'Margarita', img:'../../assets/margherita.png', price: 28.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id: 3, name: 'Pepperoni', img:'../../assets/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id: 4, name: 'Pepperoni', img:'../../assets/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id: 5, name: 'Pepperoni', img:'../../assets/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id: 6, name: 'Pepperoni', img:'../../assets/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id: 7, name: 'Pepperoni', img:'../../assets/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id: 8, name: 'Pepperoni', img:'../../assets/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id: 9, name: 'Pepperoni', img:'../../assets/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id: 10, name: 'Pepperoni', img:'../../assets/pepperoni.png', price: 35.99, sizes:['30cm', '40cm', '55cm'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
 ]
